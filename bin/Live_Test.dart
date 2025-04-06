2a24a36953dd981d85a06df0de9db2d4645ee09f
hamja95 <amirhamga78@gmail.com>
2025-04-06 23:49:16 +0600

    Live_Test.dart

---
+++ bin/Live_Test.dart
@@ -0,0 +1,29 @@
+void main(){
+
+  List<Map<String,dynamic>>students=[
+    {"name":"Alice","scores":[85,90,78]},
+    {"name":"Bob","scores":[88,76,95]},
+    {"name":"Charlie","scores":[90,92,85]}
+  ];
+
+ Map<String,double>averageScores={};
+
+ for(var student in students ){
+   String name=student["name"];
+   List<int> scores= student["scores"];
+   int sum=0;
+   for(var score in scores){
+     sum +=score;
+   }
+   double average=sum/scores.length;
+
+   averageScores[name]=double.parse(average.toStringAsFixed(2));
+
+ }
+ var sortedAverageScores=Map.fromEntries(
+   averageScores.entries.toList()..sort((e1,e2) => e2.value.compareTo(e1.value))
+ );
+
+ print(sortedAverageScores);
+
+}
